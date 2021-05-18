2fabdcfd545742a2abc6d3c3c0d46c9cf75f1dd8
ghidyon <gideonakunana@gmail.com>
2021-05-18 20:53:02 +0100

    declared schema model

---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const userSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String
+    },
+    userRole: {
+        type: String,
+        enum: ['admin', 'tutor', 'student', 'not assigned'],
+        default: "not assigned"
+    },
+    isTutor: {
+        type: Boolean,
+        default: 0
+    },
+    isAdmin: {
+        type: Boolean,
+        default: 0
+    }
+}, { timestamps: true });
