e4c8f24850d49acc0cc5b578e84d6e9992839295
ghidyon <gideonakunana@gmail.com>
2021-05-19 15:59:12 +0100

    added auth

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,9 +6,26 @@ module.exports = (req, res, next) => {
     // Get token from header
     const token = req.header('x-auth-token');
 
+    // check if token doesn't exists
     if (!token)
         return res.status(401).json({
             statusCode: 401,
-            message: 'No token, Authorization denied!'
+            message: 'No token, authorization denied!'
         });
+    
+    // else token exists
+    try {
+        // Decode our token
+        const decoded = jwt.verify(token, SECRET);
+
+        // Assign user to the request object
+        req.user = decoded.user;
+        
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            statusCode: 401,
+            message: 'Token is invalid!'
+        });
+    }
 }
