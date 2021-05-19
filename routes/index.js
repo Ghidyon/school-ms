34c7ec3a40ff7a80733b31f9d790b779489a043a
ghidyon <gideonakunana@gmail.com>
2021-05-19 17:02:14 +0100

    added get request

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const { check } = require('express-validator');
 
 // import the router controllers
 const usersController = require('../controllers/usersController');
+
+// import the authorization middleware
+const auth = require('../middleware/auth');
+
+// login user route
+router.post(
+    '/api/auth/login',
+    [
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'A valid password is required').exists()
+    ],
+    usersController.loginUser
+);
+
+// get logged in user
+router.get('api/auth', auth, usersController.getLoggedInUser);
+
+module.exports = router;
